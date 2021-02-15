97e6081980c008dc74c7b61b8db914a8a7dd633b
b6320502517 <sarannut.l@ku.th>
2021-02-15 19:36:19 +0700

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,4 +1,19 @@
 #include<stdio.h>
 int main(){
-	int n,p,a,i;
+	int n,p,a,i,k=0,l;
+	unsigned int t[48]={};
+	if(n>=1&&n<=10000)
+		scanf("%d",&n);
+	for(i=0;i<n;i++){
+		scanf("%d",&a,&p);
+		t[a-1]+=p;
+	}
+	for(i=0;i<48;i++){
+		if(k<t[i]){
+			k=t[i];
+			l=i;
+		}
+	}
+	printf("%d %ld",l+1,t[l]);
+	return 0;
 }
