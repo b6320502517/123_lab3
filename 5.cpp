ebf0788fe88c699d2477c2d21398e7a680b446c5
b6320502517 <sarannut.l@ku.th>
2021-02-15 17:44:23 +0700

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main(){
-	int n=1,i,j=0,k;
-	int c[4]={};
-	//if(n>0&&n<10000)
+	unsigned int n=1,i,j=0,k,l,m=0;
+	int c[10];
+	if(n>0&&n<10000)
 		scanf("%d",&n);
 	k=n;
 	for(i=2;n!=1;i++)
@@ -10,9 +10,14 @@ int main(){
 			c[j]=i;
 			n=n/i;
 		}
-			
+	j--;	
+	l=j;
+	for(i=0;i<j;i++)
+		for(l=j;l>0;l--)
+			if(c[i]==c[l])
+				m++;
 		
-	if(c[0]!=c[1]&&c[0]!=c[2]&&c[1]!=c[2])
+	if(m==0)
 		printf("%d is a Lucky Number",k);
 	else
 		printf("%d is not a Lucky Number",k);
