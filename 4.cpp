9aab1d3012f9d9e56330836435ab4dc07d2e5777
b6320502517 <sarannut.l@ku.th>
2021-02-15 18:48:46 +0700

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -6,10 +6,9 @@ int main(){
 		scanf("%ld",&x);
 	if(x==0||x==1)
 		printf("2");
-	while(x>2)
+	while(x>1)
 	if((x%2==0||x%3==0||x%5==0||x%7==0)&&(x!=2 && x!=3 && x!=5 && x!=7))
-		x--;
-		
+		x--;	
 				
 	else{
 		printf("%ld ",x);
