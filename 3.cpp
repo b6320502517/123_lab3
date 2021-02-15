c4f7e468b874d2bb15ed3396b2cc78e68c7efab3
b6320502517 <sarannut.l@ku.th>
2021-02-15 17:49:02 +0700

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	unsigned int k=1,i,;
+	if(n>=1&&n<=1000)
+		scanf("%d",&n);
+	for(i=2;n!=1;i++)
+		for(;n%i==0;){
+			n=n/i;
+			printf("%d\n",i);
+		}
+	printf("0");
+return 0;
+}
