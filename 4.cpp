13e895845a7672bd125e4a0b9340f1350ee39bb3
b6320502517 <sarannut.l@ku.th>
2021-02-15 18:47:50 +0700

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -4,27 +4,18 @@ int main(){
 	long  x=0;
 	if(x>=0&&x<=2000000000)
 		scanf("%ld",&x);
-	while(x>=0){
-	
-	if(x%2==0||x%3==0||x%5==0||x%7==0){
+	if(x==0||x==1)
+		printf("2");
+	while(x>2)
+	if((x%2==0||x%3==0||x%5==0||x%7==0)&&(x!=2 && x!=3 && x!=5 && x!=7))
+		x--;
 		
-		if(i==0){
-			x--;
-			i++;	
-		}
-		else{
-			printf("%ld ",x);
-			break;
-		}
-				
-	}
 				
 	else{
 		printf("%ld ",x);
 		break;
 	}
-
-	}
-		
+	
+	
 	return 0;
 }
